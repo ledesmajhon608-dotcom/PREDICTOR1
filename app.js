130acb749980744c69ea3be2567307f07c5e45b7
ledesmajhon608-dotcom <ledesmajhon608@gmail.com>
2026-03-08 17:18:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,88 +1,48 @@
 const equipos = {
-
-"Liga MX":[
-"America",
-"Chivas",
-"Tigres",
-"Monterrey"
-],
-
-"MLS":[
-"Inter Miami",
-"LA Galaxy",
-"Atlanta United",
-"Seattle Sounders"
-]
-
+"Liga MX": ["America","Chivas","Tigres","Monterrey"],
+"MLS": ["Inter Miami","LA Galaxy","Atlanta United","Seattle Sounders"]
 };
 
-const ligaSelect = document.getElementById("liga");
-const localSelect = document.getElementById("local");
-const visitanteSelect = document.getElementById("visitante");
+const liga = document.getElementById("liga");
+const local = document.getElementById("local");
+const visitante = document.getElementById("visitante");
 
-ligaSelect.addEventListener("change", function(){
+liga.onchange = function(){
 
-const liga = ligaSelect.value;
+local.innerHTML = "";
+visitante.innerHTML = "";
 
-localSelect.innerHTML = '<option value="">Equipo Local</option>';
-visitanteSelect.innerHTML = '<option value="">Equipo Visitante</option>';
+const lista = equipos[liga.value];
 
-if(!equipos[liga]) return;
+if(!lista) return;
 
-equipos[liga].forEach(function(e){
+for(let i=0;i<lista.length;i++){
 
-const optionLocal = document.createElement("option");
-optionLocal.value = e;
-optionLocal.text = e;
+let option1 = document.createElement("option");
+option1.text = lista[i];
 
-const optionVisit = document.createElement("option");
-optionVisit.value = e;
-optionVisit.text = e;
+let option2 = document.createElement("option");
+option2.text = lista[i];
 
-localSelect.appendChild(optionLocal);
-visitanteSelect.appendChild(optionVisit);
+local.add(option1);
+visitante.add(option2);
 
-});
+}
 
-});
+}
 
 function calcular(){
 
-const local = localSelect.value;
-const visitante = visitanteSelect.value;
-
-if(local === "" || visitante === ""){
-alert("Selecciona los equipos");
-return;
-}
-
-if(local === visitante){
-alert("Los equipos deben ser diferentes");
-return;
-}
+const equipoLocal = local.value;
+const equipoVisitante = visitante.value;
 
 const probLocal = Math.floor(Math.random()*50)+30;
 const probVisita = 100 - probLocal;
 
-const goles = (Math.random()*3).toFixed(1);
-const ambos = Math.random() > 0.5 ? "SI" : "NO";
-const corners = Math.floor(Math.random()*10)+5;
+document.getElementById("resultado").innerHTML =
 
-document.getElementById("resultado").innerHTML = `
-
-<div class="card">
-
-<h2>${local} vs ${visitante}</h2>
-
-<p>🏆 Probabilidad Local: ${probLocal}%</p>
-<p>✈️ Probabilidad Visita: ${probVisita}%</p>
-
-<p>⚽ Goles esperados: ${goles}</p>
-<p>🔥 Ambos marcan: ${ambos}</p>
-<p>🚩 Corners estimados: ${corners}</p>
-
-</div>
-
-`;
+"<h2>"+equipoLocal+" vs "+equipoVisitante+"</h2>"+
+"<p>Probabilidad Local: "+probLocal+"%</p>"+
+"<p>Probabilidad Visita: "+probVisita+"%</p>";
 
 }

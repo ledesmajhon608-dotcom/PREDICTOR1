10d069ef496facad34332e3a6cd26fdd682930b8
ledesmajhon608-dotcom <ledesmajhon608@gmail.com>
2026-03-22 16:40:58 -0500

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'predictor-v1';
-const assets = ['./', './index.html', './styles.css', './app.js', './manifest.json'];
+const CACHE_NAME = 'elite-v8.5';
+const urls = ['./', './index.html', './styles.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urls)));
 });
 
 self.addEventListener('fetch', e => {
